660194bc7ea7dc9417d02c0fb9be3e8a9b36f0cb
RoyDevTEAM <roidercito12345@gmail.com>
2024-08-26 15:34:59 -0400

    modal

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7e6aa2',  // Violeta claro
+        secondary: '#4a3f69',  // Violeta oscuro
+        accent: '#b794f4',  // Lila
+        highlight: '#bf843b',  // Amarillo oscuro
+        background: '#f3f4f6',  // Color de fondo claro
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],  // Fuente principal
+        serif: ['Merriweather', 'serif'],  // Fuente para encabezados o bloques de texto especiales
+      },
+      spacing: {
+        '128': '32rem',  // Espaciado extra grande
+        '144': '36rem',  // Espaciado aún más grande
+      },
+      borderRadius: {
+        '4xl': '2rem',  // Radio de borde personalizado
+      },
+    },
   },
   plugins: [],
 }
